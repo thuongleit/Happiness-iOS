a056a11f078e0aac415e7183e64aeea968c736a8
James Zhou <zhouzihan2014@gmail.com>
2016-11-12 03:05:18 -0800

    add some code for login/signup

--- Happiness/AppDelegate.swift
+++ Happiness/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let initialViewController = InitialViewController(nibName: "InitialViewController", bundle: nil)
+        let initialNavigationController = UINavigationController(rootViewController: initialViewController)
+        initialNavigationController.isNavigationBarHidden = true
+        
+        // override this line to jump directly to a specific View Controller for easy testing
+        window?.rootViewController = initialNavigationController
         return true
     }
 
---
+++ Happiness/LoginSignupViewController.swift
@@ -0,0 +1,95 @@
+//
+//  SignupViewController.swift
+//  Happiness
+//
+//  Created by James Zhou on 11/12/16.
+//  Copyright © 2016 Team Happiness. All rights reserved.
+//
+
+import UIKit
+
+class LoginSignupViewController: UIViewController {
+    
+    @IBOutlet weak var usernameView: UIView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var emailView: UIView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var loginSignupButton: UIButton!
+    
+    
+    var isSignup: Bool!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if (!isSignup) {
+            usernameView.isHidden = true
+            loginSignupButton.setTitle("Log In", for: .normal)
+        }
+        
+        setupContainerView()
+        setupTextLabel()
+        setupTextField()
+        setupButton()
+        
+        backButton.addTarget(self, action: #selector(onBack), for: .touchUpInside)
+        loginSignupButton.addTarget(self, action: #selector(onLoginSignup), for: .touchUpInside)
+    }
+    
+    func setupContainerView() {
+        for view in [usernameView, emailView, passwordView] {
+            UIConstants.setupLoginSignupContainerView(view: view!)
+        }
+    }
+    
+    func setupTextLabel() {
+        for textlabel in [usernameLabel, emailLabel, passwordLabel] {
+            textlabel?.textColor = UIConstants.secondaryThemeColor
+            textlabel?.font = UIFont(name: UIConstants.textFontName, size: 17)
+        }
+    }
+    
+    func setupTextField() {
+        UIConstants.setupLoginSignupTextField(textField: usernameTextField, withPlaceholder: "Name")
+        UIConstants.setupLoginSignupTextField(textField: emailTextField, withPlaceholder: "Email Address")
+        UIConstants.setupLoginSignupTextField(textField: passwordTextField, withPlaceholder: "Password")
+    }
+    
+    func setupButton() {
+        
+        for button in [backButton, loginSignupButton] {
+            button?.titleLabel?.font = UIFont(name: UIConstants.textFontName, size: 16)
+            button?.layer.cornerRadius = 5
+            button?.layer.masksToBounds = true
+            button?.backgroundColor = UIConstants.secondaryThemeColor
+            button?.setTitleColor(UIConstants.primaryThemeColor, for: .normal)
+        }
+    }
+
+    func onBack() {
+        _ = self.navigationController?.popViewController(animated: true)
+    }
+    
+    func onLoginSignup() {
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
