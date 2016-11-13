f364e6c61263e89ab14db3a7215c67e47166860d
Carina Boo <cboo@cboo-macbookpro.roam.corp.google.com>
2016-11-12 18:38:15 -0800

    Add QuestionsList class to getCurrentQuestion()

--- Happiness/Question.swift
+++ Happiness/Question.swift
@@ -12,10 +12,34 @@ class Question: NSObject {
     
     var id: Int?
     var text: String?
+
+}
+
+class QuestionsList: NSObject {
     
-    init(dictionary: Dictionary<String, AnyObject>) {
-        
+    static var currentQuestionIndex = 0
+    
+    static let questions: [NSString] = [
+        "What are you most grateful for today?",
+        "Find out what your coworkers' favorite food/treat is. What are they?",
+        "Who helped you out today?",
+        "Go outside and immerse yourself in nature for at least 20 min. What did you see and hear?",
+        "What did you do well today?",
+        "Connect with someone new and find out their story. Write about it."
+    ]
+
+    class func getCurrentQuestion() -> Question {
+        return getNextQuestion(0)
     }
-    
-    
+
+    class func getNextQuestion(_ i: Int) -> Question {
+        let question = Question()
+        question.text = questions[currentQuestionIndex + i] as String
+        return question
+    }
+
+    class func markCurrentQuestionCompleted() {
+        currentQuestionIndex += 1
+    }
+
 }
